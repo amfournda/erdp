dc88973ac916ef74b2a44e3fc0683899edb3d849
Erin <dan724@gmail.com>
2015-04-23 13:19:12 -0400

    Really fix the gladfile pathing problem

--- erdp.c
+++ erdp.c
@@ -19,10 +19,15 @@ int main(int argc, char *argv[]) {
 	sprintf(path, "/proc/%d/exe", pid);
 	if (readlink(path, path, PATH_MAX) == -1)
 		perror("readlink");
-	len = strlen(path);
-	path[len-11] = '\0';
+	/*this is bad and hacky and I should feel bad*/
+	len = strlen(path) - 6;
+	printf("Gladefile Path: %s\n", path);
+	int i=0;
+	for(i=len;path[i] != '/';i--);
+	path[i+1] = '\0';
 	char *gladefile = malloc(strlen(path)+strlen("erdp.glade"));
 	gladefile = g_strconcat(path, "erdp.glade", NULL);
+	printf("Gladefile Path: %s\n", gladefile);
 
 	/*get all the objects we need*/
 	gtk_init(&argc,&argv);
@@ -34,6 +39,7 @@ int main(int argc, char *argv[]) {
 	/*connect all my signal handlers and unmap my builder*/
 	gtk_builder_connect_signals(builder, NULL);
 	g_object_unref(G_OBJECT(builder));
+	free(gladefile);
 
 
 	/*set the window hint and show it*/

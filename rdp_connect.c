798313ce279a420948c2434f05354b3d8e153b38
Erin <dan724@gmail.com>
2015-04-23 15:53:09 -0400

    Add fullscreen option support

--- rdp_connect.c
+++ rdp_connect.c
@@ -5,6 +5,7 @@ void rdp_connect(GtkButton *connect, gpointer erdp) {
 	GtkEntry *rip = (GtkEntry *) find_child(erdp, "address");
 	GtkEntry *ruser = (GtkEntry *) find_child(erdp, "user");
 	GtkEntry *rpass = (GtkEntry *) find_child(erdp, "pass");
+	GtkCheckButton *fullscreen = (GtkCheckButton *) find_child(erdp, "fullscreen");
 
 	/*format my strings correctly*/
 	char *fip = malloc(strlen("/v:")+strlen(gtk_entry_get_text(rip))+1);
@@ -13,10 +14,20 @@ void rdp_connect(GtkButton *connect, gpointer erdp) {
 	fuser = g_strconcat("/u:", gtk_entry_get_text(ruser), NULL);
 	char *fpass = malloc(strlen("/p:")+strlen(gtk_entry_get_text(rpass))+1);
 	fpass = g_strconcat("/p:", gtk_entry_get_text(rpass), NULL);
+	
+	/*check what options to add to rdp*/
+	char *opts = malloc(strlen("/cert-ignore ")+1);
+	opts = g_strconcat("/cert-ignore ", NULL);
+	if(gtk_toggle_button_get_active((GtkToggleButton*)fullscreen) == TRUE) {
+		opts = realloc(opts, strlen("/f ")+strlen(opts)+1);
+		opts = g_strconcat(opts, "/f ", NULL);
+	}
+	int len = strlen(opts);
+	opts[len-1] = '\0';
 
 	/*and call xfreerdp*/
-	g_printf("Calling xfreerdp with arguments: %s %s %s\n", fip, fuser, fpass);
-	execl("/usr/bin/xfreerdp", "/cert-ignore", fip, fuser, fpass, NULL);
+	g_printf("Calling xfreerdp with arguments: %s %s %s %s\n", opts, fip, fuser, fpass);
+	execl("/usr/bin/xfreerdp", opts , fip, fuser, fpass, NULL);
 	/*code never gets here*/
 	return;
 }

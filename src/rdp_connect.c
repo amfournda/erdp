355b05d437a4bd66b9e23414fdb6a9978be18ff6
Erin Skelton <dan724.gmail.com>
2015-05-01 19:15:01 -0400

    Fixed memory leaks with options passing

--- src/rdp_connect.c
+++ src/rdp_connect.c
@@ -19,29 +19,46 @@ void rdp_connect(GtkButton *connect, gpointer erdp) {
 	char *fpass = g_strconcat("/p:", gtk_entry_get_text(rpass), NULL);
 	
 	/*check what options to add to rdp from the options tickboxes*/
-	int optslen = 4;
+	int optslen = 5;
 	char **opts = malloc(optslen * sizeof(char*));
-	opts[0] = "/usr/bin/xfreerdp";
-	opts[1] = "+cert-ignore";
-	opts[2] = fip;
-	opts[3] = fuser;
-	opts[4] = fpass;
-
+	opts[0] = malloc(sizeof(char) * (strlen("/usr/bin/xfreerdp")+1));
+	strcpy(opts[0], "/usr/bin/xfreerdp");
+	opts[1] = malloc(sizeof(char) * (strlen("/cert-ignore")+1));
+	strcpy(opts[1], "/cert-ignore");
+	opts[2] = malloc(sizeof(char) * (strlen(fip)+1));
+	strcpy(opts[2], fip);
+	opts[3] = malloc(sizeof(char) * (strlen(fuser)+1));
+	strcpy(opts[3], fuser);
+	opts[4] = malloc(sizeof(char) * (strlen(fpass)+1));
+	strcpy(opts[4], fpass);
+	
 	if(gtk_toggle_button_get_active((GtkToggleButton*)fullscreen) == TRUE) {
+		opts = (char **) realloc(opts, sizeof(char *) * (optslen+1));
+		opts[optslen] = malloc(sizeof(char) * (strlen("/f")+1));
+		strcpy(opts[optslen], "/f");
 		optslen++;
-		opts[optslen] = "/f";
 	}
 	if(gtk_toggle_button_get_active((GtkToggleButton*)decorations) == TRUE) {
+		opts = (char **) realloc(opts, sizeof(char *) * (optslen+1));
+		opts[optslen] = malloc(sizeof(char) * (strlen("/disp")+1));
+		strcpy(opts[optslen], "/disp");
 		optslen++;
-		opts[optslen] = "/disp";
+		opts = (char **) realloc(opts, sizeof(char *) * (optslen+1));
+		opts[optslen] = malloc(sizeof(char) * (strlen("/fonts")+1));
+		strcpy(opts[optslen], "/fonts");
 		optslen++;
-		opts[optslen] = "/fonts";
+		opts = (char **) realloc(opts, sizeof(char *) * (optslen+1));
+		opts[optslen] = malloc(sizeof(char) * (strlen("/aero")+1));
+		strcpy(opts[optslen], "/aero");
 		optslen++;
-		opts[optslen] = "/aero";
+		opts = (char **) realloc(opts, sizeof(char *) * (optslen+1));
+		opts[optslen] = malloc(sizeof(char) * (strlen("/window-drag")+1));
+		strcpy(opts[optslen], "/window-drag");
 		optslen++;
-		opts[optslen] = "/window-drag";
+		opts = (char **) realloc(opts, sizeof(char *) * (optslen+1));
+		opts[optslen] = malloc(sizeof(char) * (strlen("/menu-anims")+1));
+		strcpy(opts[optslen], "/menu-anims");
 		optslen++;
-		opts[optslen] = "/menu-anims";
 	}
 	if(gtk_toggle_button_get_active((GtkToggleButton*)smartscaling) == TRUE) {
 		GdkRectangle *workarea = g_new(GdkRectangle, 1);
@@ -52,25 +69,34 @@ void rdp_connect(GtkButton *connect, gpointer erdp) {
 		int height = workarea->height*0.95;
 		char sizebuff[32];
 		snprintf(sizebuff, 31, "/size:%dx%d", width, height);
+		opts = (char **) realloc(opts, sizeof(char *) * (optslen+1));
+		opts[optslen] = malloc(sizeof(char) * (strlen(sizebuff)+1));
+		strcpy(opts[optslen], sizebuff);
 		optslen++;
-		opts[optslen] = sizebuff;
 	}
 	if(gtk_toggle_button_get_active((GtkToggleButton*)sound) == TRUE) {
+		opts = (char **) realloc(opts, sizeof(char *) * (optslen+1));
+		opts[optslen] = malloc(sizeof(char) * (strlen("/sound")+1));
+		strcpy(opts[optslen], "/sound");
 		optslen++;
-		opts[optslen] = "/sound";
 	}
 	if(gtk_toggle_button_get_active((GtkToggleButton*)clipboard) == TRUE) {
+		opts = (char **) realloc(opts, sizeof(char *) * (optslen+1));
+		opts[optslen] = malloc(sizeof(char) * (strlen("/clipboard")+1));
+		strcpy(opts[optslen], "/clipboard");
 		optslen++;
-		opts[optslen] = "/clipboard";
 	}
 	if(gtk_toggle_button_get_active((GtkToggleButton*)homedir) == TRUE) {
+		opts = (char **) realloc(opts, sizeof(char *) * (optslen+1));
+		opts[optslen] = malloc(sizeof(char) * (strlen("/home-drive")+1));
+		strcpy(opts[optslen], "/home-drive");
 		optslen++;
-		opts[optslen] = "/home-drive";
 	}
 	
 
 	/* Add the trailing NULL */
-	optslen++;
+	opts = (char **) realloc(opts, sizeof(char *) * (optslen+1));
+	opts[optslen] = malloc(sizeof(NULL));
 	opts[optslen] = NULL;
 
 
